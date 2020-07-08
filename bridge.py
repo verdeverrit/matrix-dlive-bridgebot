078b9aaf9413a3a4d655c2c611938be9f548c82f
verrit <67809872+verdeverrit@users.noreply.github.com>
2020-07-08 13:24:23 +1000

    skeleton

--- bridge.py
+++ bridge.py
@@ -1,23 +1,12 @@
-#! /usr/bin/env python
-#
-# This is a bridgebot bridge for matrix and dlive chat
-# Written for personal-ish use so there's no warranty etc
-# Don't blame me if your shit breaks
-# written by verrit
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
+# THIS CODE IS NOWHERE NEAR COMPLETION
+# AS YOU CAN SEE...
+# THIS IS CURRENTLY JUST A SKELETON
+# 
+# API CALLS CURRENTLY ARE BEING TESTED
+# MESSAGE PROCESSING IS CURRENTLY BEING TESTED
+# 
+# THIS FILE WILL NOT BE UPDATED UNTIL
+# ALL TESTS ARE COMPLETED AND THE CODING BEGINS
 
 import requests
 import graphene
